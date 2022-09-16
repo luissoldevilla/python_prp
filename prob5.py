6671b34ac47a791054b26939348f008d0a423ce1
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-16 19:22:07 -0400

    prob 5: starting

---
+++ prob5.py
@@ -0,0 +1,11 @@
+# Problem 5
+
+hours1 = 6
+hours2 = 10
+hours3 = 15
+
+speed = 70
+
+print('The distance the car will travel in 6 hours', speed * hours1)
+print('The distance the car will travel in 10 hours', speed * hours2)
+print('The distance the car will travel in 15 hours', speed * hours3)
