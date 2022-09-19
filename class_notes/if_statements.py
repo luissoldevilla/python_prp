d9548982a49b3cd17667adb0b66da0a281fc4116
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-19 19:42:24 -0400

    Problem 2

--- class_notes/if_statements.py
+++ class_notes/if_statements.py
@@ -6,3 +6,13 @@ if (testScore >= 60):
     print('Pass')
 else:
     print('Fail')
+
+# Problem 2
+
+number = int(input('Number:'))
+
+
+if (number < 0):
+    print('its a negative number')
+else:
+    print('Its a postive number')
