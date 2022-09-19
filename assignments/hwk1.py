e71c1d303b699b4402b021b7cba200e42db100ba
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-19 19:47:30 -0400

    Assignment 1

---
+++ assignments/hwk1.py
@@ -0,0 +1,8 @@
+# First Assigment
+
+onepencilCost = .75
+numPencil = 12
+totalCost = onepencilCost * numPencil
+tax = totalCost * .08
+finalBill = totalCost + tax
+print('This is the answer for assignment 1: ', finalBill)
