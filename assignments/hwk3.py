c4be94c9b6f57ff10ee36d31c09ff198ceb12f2f
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-20 19:49:59 -0400

    Assignment 3

---
+++ assignments/hwk3.py
@@ -0,0 +1,12 @@
+# Third Assignment
+
+pencil = .75
+eraser = .25
+pen = 29.95
+numPencil = 6
+numEraser = 3
+numPens = 4
+totalCost3 = pencil * numPencil + eraser * numEraser + pen * numPens
+tax3 = totalCost3 * .08
+stationeryBill = totalCost3 + tax
+print('This is the answer for assignment 3: ', stationeryBill)
