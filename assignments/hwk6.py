071146094ba04fbf78cff3bbb5e02d2f4abfa0c6
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-21 19:55:48 -0400

    Assignment 6

---
+++ assignments/hwk6.py
@@ -0,0 +1,8 @@
+# Sixth Assigment
+
+eUsedAmount = 75
+eUnitCost = 10
+eBill = eUnitCost * eUsedAmount
+tax5 = eBill * .08
+finalBill3 = eBill + tax5
+print('This is the answer for assignment 6: ', finalBill3)
