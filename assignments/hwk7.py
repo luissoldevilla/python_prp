dc1926783f4022280a7756dcc9b29cf7766dd347
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-22 19:58:26 -0400

    Assignment 7

---
+++ assignments/hwk7.py
@@ -0,0 +1,9 @@
+# Seventh Assignment
+
+numCredits = 3
+tutionFee = 400
+registrationFee = 140
+allCost = numCredits * tutionFee + registrationFee
+tax6 = allCost * .08
+schoolBill =  allCost + tax6
+print('This is the answer for assignment 7: ', schoolBill)
