d2dbf814f8f5ae5aee883d085529c1945231de8e
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-22 19:59:51 -0400

    Assignment 8

---
+++ assignments/hwk8.py
@@ -0,0 +1,11 @@
+# Eight Assigment
+
+length = 10
+width = 15
+showupFee = 20
+sqf = .25
+area = length * width
+areaCost = area * sqf + showupFee
+tax7 = areaCost * .05
+estimateBill = areaCost * tax7
+print('This is the answer for assignment 8: ', estimateBill)
