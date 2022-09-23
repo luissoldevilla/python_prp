0acae6c982e4c4402f1be91c0e8755b09a500216
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-22 20:01:15 -0400

    Assignment 8: correction

--- assignments/hwk8.py
+++ assignments/hwk8.py
@@ -6,6 +6,7 @@ showupFee = 20
 sqf = .25
 area = length * width
 areaCost = area * sqf + showupFee
-tax7 = areaCost * .05
-estimateBill = areaCost * tax7
+tax = areaCost * .05
+estimateBill = areaCost * tax
+
 print('This is the answer for assignment 8: ', estimateBill)
