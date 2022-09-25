dc9932ab8b512d129a01b9a627387519598ae1b8
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-24 23:37:58 -0400

    HMk 4

---
+++ assignments/hwk4.py
@@ -0,0 +1,10 @@
+# Forth Assigment
+
+adults = 5
+seniors = 2
+adultTicket = 11
+seniorTicket = 9
+totalCost4 = adultTicket * 5 + seniorTicket * 2
+tax4 = totalCost4 * .08
+ticketBill = totalCost4 + tax4
+print('This is the answer for assignment 4: ', ticketBill)
