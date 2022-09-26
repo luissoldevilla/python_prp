dbae0f70ee39c64760f50ec68dac6782d88501bf
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-25 23:45:06 -0400

    1D pt3

--- 1d_practice.py
+++ 1d_practice.py
@@ -4,8 +4,10 @@ while(x>=1):
     x = x - 1
 
 #########
-
+# In this second iteration the counter
+# is out the loop because of the wrong indentation
+####
 x=5
 while(x>=1):
     print(x)
-    x = x - 1
+x = x - 1
