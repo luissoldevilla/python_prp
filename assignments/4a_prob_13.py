195a92746325a924ffd16699b3cca03bc04c47f6
luissoldevilla <luis.angelsoto@hotmail.com>
2022-10-20 11:37:32 -0400

    Assigment 4a prob 13

--- assignments/4a_prob_13.py
+++ assignments/4a_prob_13.py
@@ -9,3 +9,9 @@ tailCount = 0
 for counter in range(11):
 
     outcome = random.randrange(HEADS, TAILS)
+    if (outcome == HEADS):
+        print("HEADS")
+        headCount = headCount + 1
+    else:
+        print("TAILS")
+        tailCount = tailCount + 1
