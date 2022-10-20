5a9b6af40e75064c504928e67d8a63fe86f8f617
luissoldevilla <luis.angelsoto@hotmail.com>
2022-10-20 11:25:27 -0400

    Assigment 4a prob 11

---
+++ assignments/4a_prob_11.py
@@ -0,0 +1,17 @@
+import random
+
+random1 = random.randint(0, 500)
+random2 = random.randint(0, 500)
+
+print(random1, random2)
+
+user_answer = int(input('Type your answer: '))
+
+def check(random1, random2):
+    if ((random1 + random2) == user_answer):
+        print('Congrast you are right!')
+    else: print('Wrongg!!')
+
+answer = check(random1,random2)
+
+print(answer)
