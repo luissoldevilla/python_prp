b24b169adda557d9cb1e6539df994b120f24b3d8
luissoldevilla <luis.angelsoto@hotmail.com>
2022-10-24 13:28:55 -0400

    Card Game 1

---
+++ car_game1.py
@@ -0,0 +1,10 @@
+import random
+
+p1c1rank = random.randrange(1,14)
+p1c1suit = random.randrange(1,5)
+
+p1c2rank = random.randrange(1,14)
+p1c2suit = random.randrange(1,5)
+
+points = 0
+bonuspoints = 0
