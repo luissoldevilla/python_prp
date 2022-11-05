1e30304d07c0bd0492d9147ac6eee652a43eab7a
luissoldevilla <luis.angelsoto@hotmail.com>
2022-11-04 23:15:41 -0400

    Card Refact

--- card_game.py
+++ card_game.py
@@ -6,6 +6,10 @@ p1c1suit = random.randrange(1,5)
 p1c2rank = random.randrange(1,14)
 p1c2suit = random.randrange(1,5)
 
+
+# p1c2rank = random.randrange(1,14)
+# p1c2suit = random.randrange(1,5)
+
 points = 0
 bonuspoints = 0
 
