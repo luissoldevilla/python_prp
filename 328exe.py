385378d2294af58b1d29a8e30ede9d764e388d68
luissoldevilla <luis.angelsoto@hotmail.com>
2022-11-16 12:32:33 -0500

    Problem on page 328

---
+++ 328exe.py
@@ -0,0 +1,20 @@
+import random
+
+NUM_DAYS = 7
+
+
+def main():
+# Create a list to hold the sales for each day.
+    lotery_number = [0] * NUM_DAYS
+    print('Enter the sales for each day.')
+
+# Get the sales for each day.
+    for index in range(len(sales)):
+#               sales[index] = float(input(f'Day #{index + 1}: '))
+            lottery_numbers[index] = random.Randint(0, 10)
+            print('Here are the values you entered:')
+    for value in lottery_number:
+            print(value)
+# Call the main function.
+    if __name__ == '__main__':
+            main()
