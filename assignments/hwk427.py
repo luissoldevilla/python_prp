2e3dccc3422583a7adf2a38e98ed41653a1b94f8
luissoldevilla <luis.angelsoto@hotmail.com>
2022-11-16 23:55:52 -0500

    New Solution for problem 3

--- assignments/hwk427.py
+++ assignments/hwk427.py
@@ -1,11 +1,19 @@
-num_digits = 12
+def main():
+    num_months = 12
+    rain=[0]*num_months
+    print('Enter the rain for each month.')
+    for index in range(len(rain)):
+        rain[index] = float(input(f'month #{index + 1}: '))
+    print('Here are the values you entered:')
+    for value in rain:
+        print(value)
+    sum_numbers=sum(rain)
+    print('Total rain for 12 months are ',sum_numbers)
+    average=sum_numbers/12
+    print('The average rainfall per month is ',average)
+    print('The highest value is', max(rain))
+    print('The lowest value is', min(rain))
+    print('The lowest value is', min(rain))
 
-rainfall_amount = [0] * num_digits
-print('Enter ....')
-
-for index in range(len(rainfall_amount)):
-    rainfall_amount[index] = int(input('Enter the amount of rainfall'))
-    total = total + rainfall_amount[index]
-for value in rainfall_amount:
-    print(value)
-print('The lowest value is', min(rainfall_amount))
+if __name__ == '__main__':
+    main()
