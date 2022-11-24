971ecac1affd8951c44411a8bb29870852c1f984
luissoldevilla <luis.angelsoto@hotmail.com>
2022-11-24 14:46:31 -0500

    Algorithm workbench

--- algo_workbench.py
+++ algo_workbench.py
@@ -1,7 +1,8 @@
 # Write a statement that creates a dictionary 
 # containing the following key-value pairs:
 dic = {'a':1,'b':2,'c':3}
+print(dic)
 
+# Write a statement that creates an empty dictionary.
 dic2 = {}
-
 print(dic2)
