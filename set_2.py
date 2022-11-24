702b7ae7b79d1694f8a8b8e6c3773057be066a47
luissoldevilla <luis.angelsoto@hotmail.com>
2022-11-24 14:23:22 -0500

    Python Sets

--- set_2.py
+++ set_2.py
@@ -3,5 +3,9 @@ set2 = set([3,4,5,6])
 # set3 = set1.intersection(set2)
 # set3 = set1.difference(set2)
 # set3 = set2.difference(set1)
-set3 = set1.symmetric_difference(set2)
-print(set3)
+# set3 = set1.symmetric_difference(set2)
+# print(set3)
+
+# Which of the sets is a subset of the other? 
+
+# Which of the sets is a superset of the other?
