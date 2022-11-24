67ee2b2aaecb3598c9c6a7ef9e5a3d8ef0f4d6e3
luissoldevilla <luis.angelsoto@hotmail.com>
2022-11-24 14:48:14 -0500

    Algorithm workbench

--- algo_workbench.py
+++ algo_workbench.py
@@ -6,3 +6,8 @@ print(dic)
 # Write a statement that creates an empty dictionary.
 dic2 = {}
 print(dic2)
+
+# Assume the variable dct references a dictionary. 
+# Write an if statement that determines whether the key 'James' exists in the dictionary. 
+# If so, display the value that is associ- ated with that key. 
+# If the key is not in the dictionary, display a message indicating so.
