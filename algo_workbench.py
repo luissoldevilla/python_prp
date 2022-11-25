6ffd69ce8152bfc027ed14116543fec51143f643
luissoldevilla <luis.angelsoto@hotmail.com>
2022-11-25 15:00:32 -0500

    Algorithm workbench

--- algo_workbench.py
+++ algo_workbench.py
@@ -21,3 +21,7 @@ else:
 # Write an if statement that determines whether the key 'Jim' exists in the dictionary. 
 # If so, delete 'Jim' and its associated value.
 
+if 'Jim' in dic:
+    print(dic)
+else:
+    print(dic)
