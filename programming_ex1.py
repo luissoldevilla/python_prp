ba032706fa12420034fae469332e9aab13c9befa
luissoldevilla <luis.angelsoto@hotmail.com>
2022-11-26 15:28:10 -0500

    Programming Exercise 1

---
+++ programming_ex1.py
@@ -0,0 +1,13 @@
+# Write a program that creates a dictionary containing 
+# course numbers and the room numbers of the rooms 
+# where the courses meet. The dictionary should have
+# the following keyvalue pairs:
+
+# 'CS101':'3004'
+# 'CS102':'4501'
+# 'CS103':'6755'
+# 'NT110':'1244'
+# 'CM241':'1411'
+
+courseRoomNumbers = {'CS101':'3004','CS102':'4501','CS103':'6755',
+'NT110':'1244','CM241':'1411'}
