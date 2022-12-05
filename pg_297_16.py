75b09f2c133c0e0103f04ee63d519d09530bc56a
luissoldevilla <luis.angelsoto@hotmail.com>
2022-12-05 12:36:31 -0500

    Problem 16

--- pg_297_16.py
+++ pg_297_16.py
@@ -5,7 +5,8 @@ countOdd = 0
 
 for count in range(100):
     number = random.randint(0, 101)
+    print(number)
     if (number % 2) == 0:
         countEven += 1
     else:
-        
+        countOdd += 1
