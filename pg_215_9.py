c23adfcba74614bff763ac5ddb475c51172da726
luissoldevilla <luis.angelsoto@hotmail.com>
2022-12-06 15:01:08 -0500

    Problem 9

---
+++ pg_215_9.py
@@ -0,0 +1,7 @@
+ocean_level = 1
+level_raise = 1.6
+print('Year' , 1, 'ocean level is:', level_raise)
+
+for i in range(2,26):
+    ocean_level = ocean_level + level_raise
+    print('Year' , i, 'ocean level is:', ocean_level)
