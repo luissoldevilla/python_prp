1bc3f58803faaf80bb747dda88a6a65f9e9a4827
luissoldevilla <luis.angelsoto@hotmail.com>
2022-12-07 08:54:51 -0500

    Exercise 215: solving

--- pg_215_9.py
+++ pg_215_9.py
@@ -4,5 +4,5 @@ print('Year' , 1, 'ocean level is:', level_raise)
 
 for i in range(2,26):
     ocean_level = ocean_level + level_raise
-    print('Year' , i, 'ocean level is:', ocean_level)
+    # This where we print 
     print('Year' , i, 'ocean level is:', ocean_level)
