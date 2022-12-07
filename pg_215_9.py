b242f2ad6155aa4cc4c8373b5a824c1c8d63762b
luissoldevilla <luis.angelsoto@hotmail.com>
2022-12-07 09:00:17 -0500

    Exercise 215: solving

--- pg_215_9.py
+++ pg_215_9.py
@@ -1,7 +1,7 @@
 # We can start the ocean level at 0
 ocean_level = 0
 level_raise = 1.6
-print('Year' , 1, 'ocean level is:', level_raise)
+# print('Year' , 1, 'ocean level is:', level_raise)
 
 for i in range(2,26):
     ocean_level = ocean_level + level_raise
