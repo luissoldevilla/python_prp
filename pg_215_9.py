23da2da968e0ee52e3c2b661b133af24f0953d1c
luissoldevilla <luis.angelsoto@hotmail.com>
2022-12-08 09:04:35 -0500

    Exercise 215: solving

--- pg_215_9.py
+++ pg_215_9.py
@@ -1,6 +1,7 @@
 # Problem 215 # 9
 # We can start the ocean level at 0
 ocean_level = 0
+# We set a variable for the constant value of 1.6
 level_raise = 1.6
 # print('Year' , 1, 'ocean level is:', level_raise)
 
