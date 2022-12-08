20869a7d2c1707a78e37924c2716dab606c01dfc
luissoldevilla <luis.angelsoto@hotmail.com>
2022-12-08 12:36:32 -0500

    Problem 17 pg_165

--- pg_165_17.py
+++ pg_165_17.py
@@ -1,3 +1,13 @@
 print("Reboot computer and try to connect.\n(Y/N) Enter Y for yes or N for no.")
 
 answer = input("Did that fix the problem? ")
+
+if answer == "Y" or answer == "y" or answer == "n" or answer == "N":
+    if answer == "n" or answer == "N":
+        print("Reboot the router.")
+        answer = input("Did that fix the problem? ")
+
+        if answer == "Y" or answer == "y" or answer == "n" or answer == "N":
+            if answer == "n" or answer == "N":
+                print("Make sure the cables are plugged in firmly.")
+                answer = input("Did that fix the problem? ")
