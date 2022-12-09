515232600094bcb6172e8e95164b23659ad71bdb
luissoldevilla <luis.angelsoto@hotmail.com>
2022-12-09 12:42:50 -0500

    Problem 17 pg_165

--- pg_165_17.py
+++ pg_165_17.py
@@ -16,3 +16,8 @@ if answer == "Y" or answer == "y" or answer == "n" or answer == "N":
                     if answer == "n" or answer == "N":
                         print("Move the router to a new location")
                         answer = input("Did that fix the problem? ")
+
+                        if answer == "Y" or answer == "y" or answer == "n" or answer == "N":
+                            print("you need  a new router")
+                        else:
+                            print("Please enter ", Y ," for yes or ", N," for no.\n Rerun program and try again.")
