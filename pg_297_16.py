a20b1833b6dbe8c38ffaa05ccb4f559ad23cdaa6
luissoldevilla <luis.angelsoto@hotmail.com>
2022-12-14 20:57:51 -0500

    4a 11 prob

--- pg_297_16.py
+++ pg_297_16.py
@@ -8,13 +8,11 @@ import random
 
 countEven = 0
 countOdd = 0
-count4 = 0
-count5 = 0
-
 
 for count in range(100):
     number = random.randint(0, 101)
     number1 = random.randint(0, 101)
+    print(number)
     print(number)
     if (number % 2) == 0:
         countEven += 1
