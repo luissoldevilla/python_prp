f09a22cb6e42b388c76d80b8055febd20b1860c7
luissoldevilla <luis.angelsoto@hotmail.com>
2022-12-18 10:45:22 -0500

    4a 11 prob

--- assignments/4a_prob_11.py
+++ assignments/4a_prob_11.py
@@ -4,6 +4,7 @@ random1 = random.randint(0, 500)
 random2 = random.randint(0, 500)
 random3 = random.randint(0, 500)
 random4 = random.randint(0, 500)
+random5 = random.randint(0, 500)
 
 
 print(random1, random2)
