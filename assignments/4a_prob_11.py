379beb9156808812fde8012d6a4390d9f46aed7d
luissoldevilla <luis.angelsoto@hotmail.com>
2022-12-31 11:45:05 -0500

    4a 11 prob

--- assignments/4a_prob_11.py
+++ assignments/4a_prob_11.py
@@ -13,7 +13,6 @@ def check(random1, random2):
     if ((random1 + random2) == user_answer):
         print('Congrast, you are right!')
         print('You can Print message!!')
-        print('You can Print message!')
     else: print('Wrongg!!')
 
 answer = check(random1,random2)
